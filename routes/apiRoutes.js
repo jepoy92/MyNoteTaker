2d5de7c0f6ad25233d26bac796880e3bf47545cc
Jeffrey Choi <jeffrey1choi@gmail.com>
2020-09-20 19:36:53 -0700

    testing different routes in server.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,13 @@
-const db = require("../Develop/db/db.json");
+const getData = require("../Develop/db/db.json");
+
+
+
 
 module.exports = function(app) {
     
   
     app.get("/api/notes", function(req, res) {
-      res.json(db);
+      res.json(getData);
     });
   
  let id = 0;
@@ -13,15 +16,15 @@ module.exports = function(app) {
   
     app.post("/api/notes", function(req, res) {
       req.body.id = ++id;
-      db.push(req.body);
+      getData.push(req.body);
       res.json(req.body);
     
     });
 
     app.delete("/api/notes/:id", function(req, res) {
       const deleteId = req.params.id;
-      const idIndex = db.findIndex((data) => data.id == deleteId);
-      db.splice(idIndex, 1);
+      const idIndex = getData.findIndex((data) => data.id == deleteId);
+      getData.splice(idIndex, 1);
       res.json({ok: true});
     });
   
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const path = require("path")
+
 const app = express();
 
 // PORT number
-let PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
