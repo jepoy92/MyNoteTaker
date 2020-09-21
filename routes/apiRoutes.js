c860ac50189f7a29b3f450a6d17360d6888c70ec
Jeffrey Choi <jeffrey1choi@gmail.com>
2020-09-20 18:38:39 -0700

    fixed issues with connecting to db directory

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,54 +1,28 @@
-// const express = require("express");
-// const path = require("path");
-// const fs = require("fs");
-// const app = express();
+const db = require("../Develop/db/db.json");
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static(path.join(__dirname, 'db')));
-// app.use(express.static(path.join(__dirname, 'routes')));
-
-// //Routing
-
-// module.exports = function(app){
-//     app.get('/api/notes', function (request, response) {
-//         // respond with: content of db.json
-//         return response.json(JSON.parse(fs.readFileSync('./db/db.json')));
-//     });
+module.exports = function(app) {
     
-//     app.post('/api/notes', function (request, response) {
-//         // get all body stuff from request.body object
-//         // get all request parameter stuff from request.params object
-//         let newNote = request.body;
-//         let existingNotes = JSON.parse(fs.readFileSync('./db/db.json'));
-//         let id = 1;
-//         while
-//             (existingNotes.some((value, index) => {
-//                 return value.id === id
-//             })) {
-//             id++
-//         }
-//         newNote.id = id;
-//         existingNotes.push(newNote);
-//         console.log("adding new note");
-//         fs.writeFileSync('./db/db.json', JSON.stringify(existingNotes));
-//         response.json(newNote);
-//     });
-    
-//     app.delete('/api/notes/:id', function (request, response) {
-//         // get all body stuff from request.body object
-//         // get all request parameter stuff from request.params object
-//         let existingNotes = JSON.parse(fs.readFileSync('./db/db.json'));
-//         let id = parseInt(request.params.id);
-//         console.log("deleting note with id: " + id);
-//         existingNotes = existingNotes.filter(note => {
-//             return note.id !== id
-//         });
-//         console.log("updated notes are now: " + existingNotes);
-//         fs.writeFileSync('./db/db.json', JSON.stringify(existingNotes));
+  
+    app.get("/api/notes", function(req, res) {
+      res.json(db);
+    });
+  
+ let id = 0;
+  
 
-//         response.json(existingNotes);
+  
+    app.post("/api/notes", function(req, res) {
+      req.body.id = ++id;
+      db.push(req.body);
+      res.json(req.body);
     
-//     });
-// }
+    });
+
+    app.delete("/api/notes/:id", function(req, res) {
+      const deleteId = req.params.id;
+      const idIndex = db.findIndex((data) => data.id == deleteId);
+      db.splice(idIndex, 1);
+      res.json({ok: true});
+    });
+  
+  };
